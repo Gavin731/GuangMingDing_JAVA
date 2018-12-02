1dd18c74071e057371b1567c96c309992e01b6e7
曾令贵 <zenglinggui@quancheng-ec.com>
2018-12-02 16:42:35 +0800

    init

--- GuangMingDing_JAVA/app/src/main/java/com/gmd/main/activity/MainActivity.java
+++ GuangMingDing_JAVA/app/src/main/java/com/gmd/main/activity/MainActivity.java
@@ -2,12 +2,16 @@ package com.gmd.main.activity;
 
 import android.os.Bundle;
 
+import com.blankj.utilcode.util.ConvertUtils;
+import com.blankj.utilcode.util.PermissionUtils;
 import com.gmd.R;
 import com.gmd.common.base.BaseActivity;
 import com.gmd.main.adapter.TabViewPagerAdapter;
 import com.gmd.main.presenter.MainPresenter;
 import com.gmd.main.view.IMainView;
 import com.gmd.main.widget.NoTouchViewPager;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
